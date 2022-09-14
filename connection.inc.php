0ee56bf07f9d6beab989418bc0016da382335729
pratik020202 <91137425+pratik020202@users.noreply.github.com>
2022-09-15 02:50:57 +0530

    Delete connection.inc.php

--- connection.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$con=mysqli_connect("localhost","root","","avera");
-define('SERVER_PATH',$_SERVER ['DOCUMENT_ROOT'].'/php/cosmetics web/');
-define('SITE_PATH','http://localhost/php/cosmetics%20web/');
-
-define('PRODUCT_IMAGE_SERVER_PATH',SERVER_PATH.'media/product/');
-define('PRODUCT_IMAGE_SITE_PATH',SITE_PATH.'media/product/');
-?>
